57fd9cf9112d37f6b5b8a81a44b08a317fa8a297
steffen <steffen@robots.ox.ac.uk>
2024-04-29 20:20:34 +0100

    Add some docstrings

--- src/base.jl
+++ src/base.jl
@@ -2,10 +2,42 @@
 abstract type AbstractSpectralDensity end
 
 (S::AbstractSpectralDensity)(w) = error("Not implemented")
+
+"""
+    rand(S::AbstractSpectralDensity, d::Int)
+Generate a random d-dimensional sample from the spectral density `S`. 
+Because many KernelFunctions.Kernel are input dimension agnostic, you must explicitly specify the input dimension `d`.
+
+# Examples
+```jldoctest
+julia> k = SqExponentialKernel();
+
+julia> S = SpectralDensity(k);
+
+julia> rand(S, 1)
+"""
 rand(S::AbstractSpectralDensity, d::Int) = rand(Random.default_rng(), S, d)
 
 rand(S::AbstractSpectralDensity) = throw(ArgumentError("KernelFunctions.kernel are input dimension agnostic. You must explicitly specify it."))
 
+"""
+    SpectralDensity{K<:Kernel}
+Spectral density for the kernel K. 
+
+# Definition
+Given a stationary kernel ``k(x, x')`` the spectral density is the Fourier transform of ``k(τ) = k(x-x')``. 
+It can be seen as a probablity density function over the frequency domain, and can be evaluated at any frequency `w`. 
+
+# Examples
+```jldoctest
+julia> k = SqExponentialKernel();
+
+julia> S = SpectralDensity(k);
+
+julia> S(0.0)
+
+julia> rand(S, 1)
+"""
 struct SpectralDensity{K<:KernelFunctions.Kernel} <: AbstractSpectralDensity
     kernel::K
 end
