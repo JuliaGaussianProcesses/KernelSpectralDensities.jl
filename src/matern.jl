153b521b617625bb92074d79067857a20c8bb12f
steffen <steffen@robots.ox.ac.uk>
2024-06-15 13:20:51 +0100

    Use undocumented MvTDist

--- src/matern.jl
+++ src/matern.jl
@@ -17,56 +17,8 @@ end
 
 function _spectral_distribution(kernel::MaternKernels, l::AbstractVector)
     ν = _matern_order(kernel)
-    Dv = fill(TDist(2 * ν), length(l))
-    return Product(inv.(2 * π * l) .* Dv)
-end
-
-# for "compatibility" with Distributions.jl, I think I only want a constructor for 
-# the non-scaled version (i.e. MvTDist(), which sets mean to zero and Σ to eye)
-# then set the others via + / * operations
-struct MvTDist{T<:Real,Cov<:AbstractMatrix,Mean<:AbstractVector} <:
-       Distributions.ContinuousMultivariateDistribution
-    μ::Mean
-    Σ::Cov
-    ν::Int
-end
-
-import Base: length, eltype#, mean, var, cov
-
-Base.length(d::MvTDist) = length(d.μ)
-
-Base.eltype(::Type{<:MvTDist{T}}) where {T} = T
-
-# sampler(d::MvTDist) = d
-
-Distributions._rand!(::AbstractRNG, d::MvTDist, x::AbstractArray) = d
-
-# need to add SpecialFunctions.jl for loggamma
-function Distributions._logpdf(d::MvTDist, x::AbstractArray)
-    p = length(d)
-
-    t1 = loggamma((d.ν + p) / 2) - loggamma(d.ν / 2)
-    -p / 2 * (log(d.ν) + logπ)
-    -1 / 2 * logdet(d.Σ)
-
-    # log1p term not correct yet. Maybe use quadratic form from Kernelfunctions? 
-    # also missing mu
-    t2 = -(d.ν + p) / 2 * log1p(dot((x - d.μ), d.Σ, (x - d.μ)) / d.ν)
-
-    return t1 + t2
-end
-
-mean(d::MvTDist) = d.μ
-
-# suspect this is the diagonal
-function var(d::MvTDist)
-    return diag(cov(d))
-end
-
-function cov(d::MvTDist)
-    if d.ν > 2
-        return diag(d.Σ / (d.ν - 2))
-    else
-        ArgumentError("Variance is undefined for ν <= 2")
-    end
+    n = length(l)
+    l = inv.(2 * π * l) .^ 2
+    D = Distributions.MvTDist(2 * ν, zeros(n), diagm(l))
+    return D
 end
