52393adb166a3da1c888e3f80eb3fca37de6e0e2
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2025-02-13 22:09:42 +0000

    Adjust filtering

--- examples/densities/script.jl
+++ examples/densities/script.jl
@@ -42,7 +42,7 @@ for (key, ker) in kers
 end
 axislegend()
 f
-DisplayAs.Text(DisplayAs.PNG(f)) #src
+DisplayAs.PNG(f) #src #md
 
 # Now we can use a function from KernelSpectralDensities.jl to 
 # get its spectral density.
@@ -61,7 +61,7 @@ for (key, ker) in kers
 end
 axislegend()
 f
-DisplayAs.Text(DisplayAs.PNG(f)) #src
+DisplayAs.PNG(f) #src #md
 
 # ## Recovering the kernel
 # We can recover the kernel by integrating the spectral density over all frequencies.
@@ -91,4 +91,4 @@ lines!(ax, τv, k.(τv); label="kernel")
 lines!(ax, τv, ks.(τv); label="spectral approx")
 axislegend()
 f
-DisplayAs.Text(DisplayAs.PNG(f)) #src
+DisplayAs.PNG(f) #src #md
