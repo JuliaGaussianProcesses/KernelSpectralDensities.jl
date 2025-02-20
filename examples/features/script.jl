41bd7454d50cae4d9f6dd38278c15f7c19013fab
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2025-02-20 18:35:08 +0000

    Add kernel trick

--- examples/features/script.jl
+++ examples/features/script.jl
@@ -8,6 +8,7 @@
 # **Load required packages**
 using KernelSpectralDensities
 using StatsBase
+using LinearAlgebra
 using CairoMakie
 using DisplayAs #hide
 
@@ -31,10 +32,10 @@ S = SpectralDensity(ker, 1)
 #
 # It is defined as
 # ```math
-#     \varphi(x) = \sqrt{2 / l}  \cos(2  π  ((w^T  x) + b))
+#     \varphi_i(x) = \sqrt{2 / l}  \cos(2  π  ((w_i^T  x) + b_i))
 # ```
-# where `w` is sampled from the spectral density `S`, 
-# `b` is uniformly sampled from `[0, 2π]` 
+# where `w_i` is sampled from the spectral density `S`, 
+# `b_i` is uniformly sampled from `[0, 2π]` 
 # and `l` is the number of sampled frequencies, which is also 
 # the number of features.
 #
@@ -81,4 +82,41 @@ f
 DisplayAs.PNG(f) #hide #md
 
 # ## Approximating a kernel
-# Tbd
+# We can use the feature functions to approximate a kernel,
+# using the kernel trick
+# ```math
+#     k(x, x') = \langle \varphi(x), \varphi(x') \rangle
+# ```
+# 
+# To demonstrate this, we generate some feature functions and
+# see how well they recover the kernel. 
+rff = ShiftedRFF(S, 100)
+kt(x, y) = dot(rff(x), rff(y))
+
+x_plot = range(0, 2; length=50)
+f = Figure(; size=(600, 400))
+ax = Axis(f[1, 1]; xlabel="y", ylabel="ker(0., y)", title="")
+lines!(ax, x_plot, ker.(0, x_plot); label="Original Kernel")
+lines!(ax, x_plot, kt.(0, x_plot); label="KT, l = 100")
+axislegend(ax)
+f
+DisplayAs.PNG(f) #hide #md
+
+# Clearly this is not quite correct, and we can quantify this 
+# by checking the error
+
+norm(ker.(0, x_plot) .- kt.(0, x_plot))
+
+# Fortunately, we can improve the 
+# approximation by using more features
+
+rff1000 = ShiftedRFF(S, 5000)
+kt1000(x, y) = dot(rff1000(x), rff1000(y))
+
+lines!(ax, x_plot, kt1000.(0, x_plot); label="KT, l=1000")
+f
+
+# We also see that the error reduces
+norm(ker.(0, x_plot) .- kt1000.(0, x_plot))
+
+## Comparing the RFFs
